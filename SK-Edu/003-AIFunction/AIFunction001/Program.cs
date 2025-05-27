a998afb3731295b30512b7d238adf335650520b7
Kazuki Ota <kaota@microsoft.com>
2025-05-27 13:20:15 +0900

    a

---
+++ SK-Edu/003-AIFunction/AIFunction001/Program.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+// User Secrets から設定を読み込む
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+// AOAI にデプロイしているモデル名
+var modelDeploymentName = configuration["AOAI:ModelDeploymentName"] 
+    ?? throw new ArgumentNullException("AOAI:ModelDeploymentName is not set in the configuration.");
+// AOAI のエンドポイント
+var endpoint = configuration["AOAI:Endpoint"] 
+    ?? throw new ArgumentNullException("AOAI:Endpoint is not set in the configuration.");
+
+// Builder を作成
+var builder = Kernel.CreateBuilder();
+
+// AOAI 用の Chat Client を登録
+builder.AddAzureOpenAIChatClient(
+    modelDeploymentName,
+    endpoint,
+    new AzureCliCredential());
+
+// AI サービスが登録された Kernel を作成
+var kernel = builder.Build();
+
+// プロンプトから関数を作成
+var introductionFunction = KernelFunctionFactory.CreateFromPrompt(
+    new PromptTemplateConfig("""
+        <message role="system">
+          あなたは猫型アシスタントです。
+          猫らしく振舞うために語尾は「にゃん」にしてください。
+        </message>
+        <message role="user">
+          <text>
+            こんにちは！私の名前は {{$name}} です。
+            どうぞよろしくお願いします。
+            この画像に写っている動物はなんですか？
+          </text>
+          <image>https://pbs.twimg.com/profile_images/1642066284379799552/VCZ4d9Hw_400x400.jpg</image>
+        </message>
+        """)
+    {
+        InputVariables = [
+            // name パラメーターでエスケープ処理を無効にする
+            new() { Name = "name", AllowDangerouslySetContent = true },
+        ]
+    });
+
+// 実行して結果を表示
+var result = await introductionFunction.InvokeAsync(
+    kernel, 
+    new KernelArguments
+    {
+        ["name"] = "Kazuki",
+    });
+Console.WriteLine(result.GetValue<string>());
---
+++ SK-Edu/003-AIFunction/AIFunction002/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.PromptTemplates.Liquid;
+
+// User Secrets から設定を読み込む
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+// AOAI にデプロイしているモデル名
+var modelDeploymentName = configuration["AOAI:ModelDeploymentName"]
+    ?? throw new ArgumentNullException("AOAI:ModelDeploymentName is not set in the configuration.");
+// AOAI のエンドポイント
+var endpoint = configuration["AOAI:Endpoint"]
+    ?? throw new ArgumentNullException("AOAI:Endpoint is not set in the configuration.");
+
+// Builder を作成
+var builder = Kernel.CreateBuilder();
+
+// AOAI 用の Chat Client を登録
+builder.AddAzureOpenAIChatClient(
+    modelDeploymentName,
+    endpoint,
+    new AzureCliCredential());
+
+// AI サービスが登録された Kernel を作成
+var kernel = builder.Build();
+
+// プロンプトから関数を作成
+var introductionFunction = KernelFunctionFactory.CreateFromPrompt(
+    // Liquid テンプレートはループや分岐をサポートしているので複数のタグを生成可能
+    new PromptTemplateConfig("""
+        <message role="system">
+          あなたは猫型アシスタントです。
+          猫らしく振舞うために語尾は「にゃん」にしてください。
+        </message>
+        {% for message in messages %}
+        <message role="{{message.role}}">
+            {{message.text}}
+        </message>
+        {% endfor %}
+        """)
+    {
+        TemplateFormat = LiquidPromptTemplateFactory.LiquidTemplateFormat,
+    },
+    // テンプレートファクトリを指定して Liquid テンプレートを使用する
+    promptTemplateFactory: new LiquidPromptTemplateFactory());
+
+// パラメーターとして渡す ChatMessage の配列を作成
+ChatMessage[] messages = [
+    new (ChatRole.User, "こんにちは！私の名前は Kazuki です。どうぞよろしくお願いします。"),
+    new (ChatRole.Assistant, "こんにちは、Kazukiさん！よろしくお願いします。にゃん！"),
+    new (ChatRole.User, "実は私は犬なんです…、黙っててごめんなさい…。猫の敵です…。"),
+];
+
+// テンプレートに変数を設定してレンダリング
+var renderedPrompt = await introductionFunction.InvokeAsync(
+    kernel,
+    new KernelArguments
+    {
+        ["messages"] = messages,
+    });
+// レンダリングされたプロンプトを表示
+Console.WriteLine(renderedPrompt);
+
